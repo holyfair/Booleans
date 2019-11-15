e9b808649b635045958d207412eca468333b08e6
Yaroslav Kostenko <yaroslavkostenko.yk@gmail.com>
2019-11-15 14:04:22 +0200

    temp

---
+++ Booleans/Views/LimitView.xaml.cs
@@ -0,0 +1,20 @@
+﻿using Booleans.Tools.Navigation;
+using Booleans.ViewModels;
+using MahApps.Metro.Controls;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace Booleans.Views
+{
+    /// <summary>
+    /// Interaction logic for LimitView.xaml
+    /// </summary>
+    public partial class LimitView : MetroWindow
+    {
+        public LimitView()
+        {
+            InitializeComponent();
+            DataContext = new LimitViewModel(this.Close);
+        }
+    }
+}
