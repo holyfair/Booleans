bbe21ae71d5a3d475bbeaa3e710b23af821f0dfd
Yaroslav Kostenko <yaroslavkostenko.yk@gmail.com>
2019-11-19 17:01:01 +0200

    lim

--- Booleans/ViewModels/LimitViewModel.cs
+++ Booleans/ViewModels/LimitViewModel.cs
@@ -1,6 +1,8 @@
-﻿using Booleans.Tools;
+﻿using Booleans.Models;
+using Booleans.Tools;
 using Booleans.Tools.Managers;
 using Booleans.Tools.Navigation;
+using Npgsql;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,9 +13,20 @@ namespace Booleans.ViewModels
 {
     internal class LimitViewModel : BaseViewModel
     {
+        private decimal _limit;
         private RelayCommand<object> _backCommand;
+        private RelayCommand<object> _changeLimitCommand;
         private Action close;
 
+        public decimal Limit
+        {
+            get => _limit;
+            set
+            {
+                _limit = value;
+                OnPropertyChanged();
+            }
+        }
         public LimitViewModel(Action close)
         {
             this.close = close;
@@ -25,6 +38,28 @@ namespace Booleans.ViewModels
             {
                 return _backCommand ?? (_backCommand = new RelayCommand<object>(o => close()));
             }
+        }
+
+        public RelayCommand<Object> ChangeLimitCommand
+        {
+            get
+            {
+                return _changeLimitCommand ?? (_changeLimitCommand = new RelayCommand<object>(o => ChangeLimit()));
+            }
+        }
+
+        private void ChangeLimit()
+        {
+            string sql = "UPDATE \"Account\" SET " +
+                         "\"Limit\" = @limit Where \"AccountNumber\" = @accountNumber";
+            using (NpgsqlCommand command = new NpgsqlCommand(sql, ConnectionManager.GetInstance().Connection))
+            {
+                command.Parameters.AddWithValue("@limit", Limit);
+                command.Parameters.AddWithValue("@accountNumber", StationManager.DataStorage.CurrentAccount.AccountCard.AccountNumber);
+
+                command.ExecuteNonQuery();
+            }
+            close.Invoke();
         }
     }
 }
